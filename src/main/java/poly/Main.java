c2c3d2eec9cd0d28365512a41b845a29636c6838
NgocbichChu <ngocbichh2601@gmail.com>
2025-03-16 11:58:44 +0700

    add description tinh dong goi

--- src/main/java/poly/Main.java
+++ src/main/java/poly/Main.java
@@ -12,4 +12,18 @@ public class Main {
 
 
     }
+//    OOP là viết tắt của Object-oriented programming (lập trình hướng đối tượng).
+/**
+ * Các tính chất của lập trình hướng đối tượng là gì?
+ Lập trình hướng đối tượng bao gồm 4 tính chất:
+ - Tính đóng gói (**Encapsulation**)
+ - Tính kế thừa (**Inheritance**)
+ - Tính đa hình (**Polymorphism**)
+ - Tính trừu tượng (**Abstraction**)
+ * */
+
+//Class laf đại diện cho 1 đối tượng, được hiểu như 1 bản định nghĩa
+// (Object) - Đối tượng là một thể hiện của lớp. Nó bao gồm các thuộc tính và phương thức.
+
+//    private - an toàn bảo mật - truyền thì dùng getter - setter
 }
